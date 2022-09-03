968f6a1a379d2c37857a0c4964449b7970335de4
Official-simple-soul <abdurroheemsherifdeen@gmail.com>
2022-09-03 23:06:39 +0100

    new

--- js/app.js
+++ js/app.js
@@ -81,3 +81,4 @@ random.addEventListener('click', function() {
         }
 })
 
+
---
+++ js/home.js
@@ -0,0 +1,72 @@
+
+let reader = document.querySelector('.reader')
+let readerNum = document.querySelector('.reader-num')
+let letsPlay = document.querySelector('.lets-play')
+let inText = document.querySelector('.in-text')
+let set = document.querySelector('.set')
+let inputName = document.querySelector('.input-name')
+let submitName = document.querySelector('.submit-name')
+let headerBox = document.querySelector('.header-box')
+let welcomeBox = document.querySelector('.welcome-box')
+let detailsBox = document.querySelector('.details-box')
+let read = 0
+
+
+
+function myReader() {
+const readTime = setInterval(() => {
+    read++
+    reader.style.width = `${read}%`
+    readerNum.innerText = `${read}%`
+
+    // document.querySelector('.loading').classList.remove('hide')
+
+    if (read > 10 && read <= 33) {
+        inText.textContent = 'Getting ready'
+    }
+    else if (read <= 66) {
+        inText.textContent = 'Experience loads of fun'
+    }
+    else if (read <= 99) {
+        inText.textContent = 'Created by Simple-soul'
+    }
+    else {
+        inText.textContent = 'Completed'
+        document.querySelector('.loading').innerHTML = 'Completed'
+    }
+
+    if(read == 100) {
+        clearInterval(readTime)
+    }
+
+}, 100)
+}
+
+letsPlay.addEventListener('click', function() {
+    setTimeout(myReader, 2000)
+    document.querySelector('.loading').classList.remove('hide')
+    
+})
+
+
+
+submitName.addEventListener('input', function() {
+    let nameValue = inputName.value
+    set.innerText = `<p class="set">Hi ${nameValue}, Match is set for you</p>`
+})
+
+const screens = setInterval(() => {
+    read++
+
+    if (read == 90) {
+
+    }
+
+    if(read == 100) {
+        clearInterval(screens)
+        welcomeBox.classList.add("hide")
+        welcomeBox.classList.add("opacity")
+        detailsBox.classList.remove("hide")
+        headerBox.classList.remove("hide")
+    }
+}, 50)
