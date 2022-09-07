09731e72cd33014c6e8d8daf0251eef29ccfbba5
Official-simple-soul <abdurroheemsherifdeen@gmail.com>
2022-09-07 18:11:33 +0100

    new

--- js/app.js
+++ js/app.js
@@ -14,18 +14,24 @@ let readerNum = document.querySelector('.reader-num')
 let letsPlay = document.querySelector('.lets-play')
 let inText = document.querySelector('.in-text')
 let set = document.querySelector('.set')
+let set2 = document.querySelector('.details-box-computer .set')
 let inputName = document.querySelector('.input-name')
+let inputName2 = document.querySelector('.details-box-computer .input-name')
 let submitName = document.querySelector('.submit-name')
+let submitName2 = document.querySelector('.details-box-computer .submit-name')
 let main = document.querySelector('.main-3')
 let welcomeBox = document.querySelector('.welcome-box')
 let detailsBox = document.querySelector('.details-box')
+let detailsBoxComputer = document.querySelector('.details-box-computer')
 let congrat = document.querySelector('.congratulations')
+let versus = document.querySelector('.versus')
 let rules = document.querySelector('.rules')
 let validInput = document.querySelector('.valid')
+let validInput2 = document.querySelector('.details-box-computer .valid')
 let read = 0
 let getstart = 0
 
-let nameArr = []
+let nameArr = ['']
 
 
 
@@ -70,6 +76,40 @@ submitName.addEventListener('click', function() {
     
 })
 
+submitName2.addEventListener('click', function() {
+    let nameValue = inputName2.value
+     if (nameValue.length < 2) {
+             validInput2.classList.remove('hide')
+             setTimeout(() => {
+                 validInput2.classList.add('hide')
+             }, 3000)
+             set2.innerText = ''
+         }
+     else {
+         nameArr.unshift(nameValue)
+         inputName2.value = ''
+     }
+
+        inputName2.classList.add('opacity')
+        submitName2.classList.add('hide')
+        playOne.innerHTML = nameArr[0]
+        playTwo.innerHTML = 'Computer'
+        set2.innerHTML = `${nameArr[0]} vs Computer`
+        document.querySelector('.details-box-computer .enter-names').innerHTML = 'Please wait, your game is been set'
+        let gettingSet = setInterval(() => {
+            getstart++
+            document.querySelector('.details-box-computer .getting-ready').classList.remove('hide')
+            if (getstart == 100) {
+                clearInterval(gettingSet)
+                rules.classList.remove('hide')
+                detailsBoxComputer.classList.add('hide')
+            }
+        }, 50)
+
+       
+     
+ })
+
 
 
 random.addEventListener('click', function() {
@@ -93,7 +133,12 @@ random.addEventListener('click', function() {
             cardOne.innerHTML = randomNumber
             document.querySelector('.card-one-bg').innerHTML = randomNumber
             playerCountOne.innerHTML = inner
-            nextPlayer.innerHTML = `${nameArr[1]} is picking`
+            if (nameArr[1] == '') {
+                nextPlayer.innerHTML = `Computer is picking`
+            }
+            else {
+                nextPlayer.innerHTML = `${nameArr[1]} is picking`
+            }
         }
 
         // decision
@@ -101,11 +146,19 @@ random.addEventListener('click', function() {
             nextPlayer.innerHTML = `${nameArr[0]} is out of the game`
             nextPlayer.classList.add('lost')
             document.querySelector('.continue').classList.remove('hide')
-            document.querySelector('.winner-name').innerText = nameArr[1]
+            document.querySelector('.winner-name').innerText = 'Computer'
+            clearInterval(setTime)
         }
 
         if (playerCountTwo.innerHTML > 20) {
-            nextPlayer.innerHTML = `${nameArr[1]} is out of the game`
+            if (nameArr[1] == '') {
+                nextPlayer.innerHTML = `Computer is out of the game`
+            }
+
+            else {
+                nextPlayer.innerHTML = `${nameArr[1]} is out of the game`
+            }
+            
             nextPlayer.classList.add('lost')
             document.querySelector('.continue').classList.remove('hide')
             document.querySelector('.winner-name').innerText = nameArr[0]
@@ -117,16 +170,43 @@ random.addEventListener('click', function() {
             nextPlayer.classList.add('win')
             document.querySelector('.continue').classList.remove('hide')
             document.querySelector('.winner-name').innerText = nameArr[0]
+            clearInterval(setTime)
         }
 
         if (playerCountTwo.innerHTML == 20) {
-            nextPlayer.innerHTML = `${nameArr[1]} wins`
+            if (nameArr[1] == '') {
+                nextPlayer.innerHTML = `Computer wins`
+                document.querySelector('.winner-name').innerText = 'Computer'
+            }
+
+            else {
+                nextPlayer.innerHTML = `${nameArr[1]} wins`
+                document.querySelector('.winner-name').innerText = nameArr[1]
+            }
             nextPlayer.classList.add('win')
-            document.querySelector('.continue').classList.remove('hide')
-            document.querySelector('.winner-name').innerText = nameArr[1]
+            document.querySelector('.continue').classList.remove('hide') 
 
         }
+
+        if (random.classList.contains('change') && nameArr[1] == '') {
+            let reading = 0
+            let setTime = setInterval(() => {
+                reading++
+                lg(reading)
+                if(reading == 20) {
+                    random.click()
+                }
+                else if (reading > 20) {
+                    clearInterval(setTime)
+                }
+            }, 100)
+            
+            
+        }
 })
+
+   
+
 
 function allRead(n) {
     let timeOut = setInterval(() => {
@@ -187,7 +267,7 @@ function myReader() {
     setTimeout(() => {
         marginFive()
         welcomeBox.classList.add("hide")
-        detailsBox.classList.remove('hide')
+        versus.classList.remove('hide')
     }, 10000);
 
 }
@@ -211,3 +291,20 @@ document.querySelector('.rule-continue').addEventListener('click', () => {
     main.classList.remove('hide')
     rules.classList.add('hide')
 })
+
+document.querySelector('.vs-user').addEventListener('click', () => {
+    detailsBox.classList.remove('hide')
+    versus.classList.add('hide')
+})
+
+document.querySelector('.vs-computer').addEventListener('click', () => {
+    detailsBoxComputer.classList.remove('hide')
+    versus.classList.add('hide')
+})
+
+
+
+
+
+
+
